520f124f2135a911be9c37dea8bb33710561431a
lerokkkkk <136227744+lerokkkkk@users.noreply.github.com>
2023-06-27 23:48:12 +0300

    Create data.h

---
+++ c#/database/data.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void ShowData(Books* data, unsigned &count );
+void EditRecord(Books* data, unsigned &count);
+void AddRecord(Books* data, unsigned &count);
+void DeleteRecord(Books* data, unsigned &count);
+void SortRecord(Books* data, unsigned &count);
+void FindRecord(Books* data, unsigned &count);
+void AveragePrice(Books* data, unsigned &count);
